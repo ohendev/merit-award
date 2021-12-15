6ebede86f243713cbd7b80260b760c6bd6cbd7c2
Marina BOUVART <bouvartm@yahoo.fr>
2021-12-15 15:12:56 -0400

    seed fini

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Company.create(name: "La banque postale", activity: "financiere",  descrtiption:"société de financement banquaire", adresse:"40 rue des bois 97200 Fort de France" telephone: "0596757575")
+Company.create(name: "lockout ", activity: "informatique", descrtiption:"société de formation en informatique", adresse: "50 avenue des champs 97200 Fort de France" telephone: "0596908372" )
+Company.create(name: "cdsicount", activity: "vente", descrtiption:"société de vente a distance", adresse: "100 bld charles de Gaulle 97200 Fort de France" telephone: "0596363635")
+Company.create(name: "amazon", activity: "vente", descrtiption:"société de vente a distance",adresse: "01 rue de la riviere 97200 Fort de France" telephone: "0596010203" )
