8e1b0a80b1dad6d069374bd379de8d1c9099dd04
Laurent VIGNERON <laur.vigneron@gmail.com>
2021-12-15 14:30:34 -0400

    added model for company and routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_15_162230) do
+ActiveRecord::Schema.define(version: 2021_12_15_182910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "activity"
+    t.text "description"
+    t.string "address"
+    t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
