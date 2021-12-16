d5e3566715ce371046a8cfaab8bc658bf09a7f99
Laurent VIGNERON <laur.vigneron@gmail.com>
2021-12-16 16:21:07 -0400

    feature done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,35 @@
 class ReviewsController < ApplicationController
+  before_action :find_company, only: [:new, :create]
+
   def new
-    @company = Company.find(params[:company_id])
     @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.company = @company
+    @review.user = current_user
+    if @review.save
+      redirect_to company_path(@company.id) 
+    else
+      render :new
+    end
   end
-  
+
+  def destroy
+    @review = Review.find(params[:id])
+    if @review.destroy
+      redirect_to company_path(@review.company)
+    end
+  end
+
+  private
+
+  def find_company
+    @company = Company.find(params[:company_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:topic, :comment)
+  end
 end
