3b35223d85d5cb55ed1840ef805900b482a0cd94
Laurent VIGNERON <laur.vigneron@gmail.com>
2021-12-16 18:13:02 -0400

    enhanced reviews feature with stars

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :company
+  validates :topic, presence: true
+  validates :comment, presence: true
+  validates :stars, presence: true
+  validates :stars, numericality: { only_integer: true }
+  validates :stars, format: { with: /\A[0-5]\z/,
+    message: "votre note doit être entre 0 et 5." }
 end
